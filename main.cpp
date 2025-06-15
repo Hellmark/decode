0a93e411526efb1e61dbbff7448e52538924ffbf
Hellmark <spamtrap@hellmark.org>
2025-06-15 15:12:49 -0500

    Icon

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/decode.svg"));
     MainWindow window;
     window.show();
     return app.exec();
